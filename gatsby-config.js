721ad4327542a1e4b6315fdf5cbecdcd0e4b68f7
Hamid Yaftian <hamid@re-work.dev>
2022-11-09 19:20:20 +0330

    Add ESLint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  siteMetadata: {
-    title: `gatsby-blog-tutorial`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [
-    'gatsby-plugin-postcss',
-  ],
-}
+    siteMetadata: {
+        title  : 'gatsby-blog-tutorial',
+        siteUrl: 'https://www.yourdomain.tld',
+    },
+    plugins: [
+        'gatsby-plugin-postcss',
+        {
+            resolve: 'gatsby-plugin-intl',
+            options: {
+                path           : `${__dirname}/src/locales`,
+                languages      : ['en'],
+                defaultLanguage: 'en',
+                redirect       : false,
+            },
+        },
+    ],
+};
